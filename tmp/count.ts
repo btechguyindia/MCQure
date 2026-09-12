d0180a37692837ace00f9fa77d7a0bbeb9a3d8e6
Compscicode99 <nakateprathmesh999@gmail.com>
2026-09-12 21:08:27 +0530

    chore: ignore scratch tmp files

--- tmp/count.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-const p = new PrismaClient();
-async function main() {
-  const exam = await p.exam.findFirst({ where: { active: true } });
-  console.log("total:", await p.question.count({ where: { examId: exam.id } }));
-}
-main().catch((e) => console.error(e)).finally(() => p.$disconnect());
